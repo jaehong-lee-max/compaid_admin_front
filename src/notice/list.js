5dea97cbb9b92754ec0e19dcea0533ea2ca3dc36
jaehonglee <jhljjg071483@gmail.com>
2025-04-09 14:39:05 +0900

    20250409

--- src/notice/list.js
+++ src/notice/list.js
@@ -23,6 +23,7 @@ function List() {
 
   useEffect(() => {
     setMenuCheckNumber([3, 0]);
+    GetList();
   }, [currentPage]);
 
   let loading;
@@ -30,6 +31,28 @@ function List() {
   if (isLoading) {
     loading = <Loading />;
   }
+  const GetList = () => {
+    setIsLoading(true);
+    axios
+      .get(`http://localhost:3000/notice`)
+      .then((response) => {
+        console.log(response);
+        const data = response.data;
+        setListNumber(data.length); // 전체 개수 저장
+
+        // 현재 페이지에 해당하는 데이터 필터링
+        const startIndex = (currentPage - 1) * itemsPerPage;
+        const paginatedData = data.slice(startIndex, startIndex + itemsPerPage);
+
+        setList(paginatedData);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+        navigate("/login");
+      });
+  };
 
   return (
     <>
@@ -37,22 +60,32 @@ function List() {
         <h1>공지사항 리스트</h1>
         <table>
           <colgroup>
-            <col style={{ width: "90%" }} />
+            <col style={{ width: "80%" }} />
+            <col style={{ width: "10%" }} />
             <col style={{ width: "10%" }} />
           </colgroup>
           <thead>
             <tr>
               <th>제목</th>
+              <th>글쓴이</th>
               <th>날짜</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {list.map((data, i) => (
+              <tr key={i}>
+                <td style={{ textAlign: "left" }}>{data.title}</td>
+                <td>관리자</td>
+                <td>{data.createdAt.split("T")[0]}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        {/* <Pagination
+        <Pagination
           listNumber={listNumber}
           itemsPerPage={itemsPerPage}
           onPageChange={setCurrentPage}
-        /> */}
+        />
       </div>
 
       {loading}
